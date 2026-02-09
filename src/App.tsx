4a1ead967eee392915b35e72974e7853c2bde707
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-09 15:22:45 +0100

    store exported with dispatch and store state type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
-
+import './App.css';
+import store from './store/store';
+import { Provider } from 'react-redux';
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+        < Provider store={store}>
+         <div></div>
+        </Provider>
+  );
 }
 
 export default App
